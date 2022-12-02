0af1f7a82facfc64814fac8a3d46e3be98ed8e12
Mendri <marcalmendrighi@gmail.com>
2022-12-02 06:23:37 -0300

    feat: tela de login ou cadastro feita

--- frontend/src/components/GenericEmailPassForm.jsx
+++ frontend/src/components/GenericEmailPassForm.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-function GenericEmailPassForm({ type }) {
+function GenericEmailPassForm({ title, type, className, hasName = false }) {
 	return(
-		<form>
-			<label htmlFor={`${type}-email`}>
-            Email
-				<input type="email" id={`${type}-email`} />
-			</label>
-			<label htmlFor={`${type}-password`}>
-            Password
-				<input type="password" id={`${type}-password`} />
-			</label>
-			<button>{ `${type[0].toUpperCase() + type.substring(1)}` }</button>
+		<form className={className}>
+			<h3 className="text-xl font-semibold">{ title }</h3>
+			{
+				hasName
+				&& <input className="border-2 border-solid rounded p-1" placeholder="Username" id={`${type}-name`} />
+			}
+			<input className="border-2 border-solid rounded p-1" placeholder="Email" id={`${type}-email`} />
+			<input className="border-2 border-solid rounded p-1" placeholder="Password" id={`${type}-password`} />
+			<button className="w-24 p-1 rounded-xl text-gray-50 bg-emerald-600">{`${type[0].toUpperCase() + type.substring(1)}`}</button>
 		</form>
 	);
 }
@@ -21,5 +20,8 @@ function GenericEmailPassForm({ type }) {
 export default GenericEmailPassForm;
 
 GenericEmailPassForm.propTypes = {
+	hasName: PropTypes.bool,
 	type: PropTypes.string,
+	className: PropTypes.string,
+	title: PropTypes.string,
 }.isRequired;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,9 +3,9 @@ import GenericEmailPassForm from "../components/GenericEmailPassForm";
 
 function Login() {
 	return (
-		<section className="flex">
-			<GenericEmailPassForm className="email-pass-form" type="login" />
-			<GenericEmailPassForm className="email-pass-form" type="register"/>
+		<section className="flex justify-evenly items-center h-full w-full bg-slate-100">
+			<GenericEmailPassForm title="Já possui uma conta?" className="flex flex-col justify-evenly items-center w-4/12 h-2/3 shadow-2xl rounded-md bg-white" type="login" />
+			<GenericEmailPassForm title="Crie uma conta" className="flex flex-col justify-evenly items-center w-4/12 h-2/3 shadow-2xl rounded-md bg-white" type="register" hasName={ true } />
 		</section>
 	);
 }
